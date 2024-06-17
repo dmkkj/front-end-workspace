6eec7a6b3398b3a8c260ec3432eb3118c5075d63
dmkkj <40270618+dmkkj@users.noreply.github.com>
2024-06-17 16:36:50 +0900

    20240617

--- 3_JavaScript/연습.js
+++ 3_JavaScript/연습.js
@@ -1,8 +1,3 @@
-/*
-    사용자가 입력을 했을 때
-    해당 조건이 틀릴 경우 빨간색으로 표시
-               맞는 경우 초록색 표시와 함께 OK!
-*/
 const userId = document.querySelector("#userId");
 const userIdSpan = document.querySelector("#userIdSpan");
 const userPwd = document.querySelector("#userPwd");
@@ -14,29 +9,24 @@ const userNameSpan = document.querySelector("#userNameSpan");
 const email = document.querySelector("#email");
 const emailSpan = document.querySelector("#emailSpan");
 
-userId.addEventListener("input", function (e) {
-  // 첫글자는 반드시 영문자로, 그리고 영문자, 숫자 포함하여 총 4~12자로 입력하시오.
+// 첫글자는 반드시 영문자로, 그리고 영문자, 숫자 포함하여 총 4~12자로 입력하시오.
+userId.addEventListener("input", function () {
   const regExp = /^[a-zA-Z][a-zA-Z0-9]{3,11}$/;
   const check = regExp.test(userId.value);
-  //console.log(e.target.nextElementSibling);
+
   if (check) {
     userIdSpan.style.color = "green";
     userIdSpan.innerHTML = "OK!";
-    //e.target.nextElementSibling.style.color = "green";
-    //e.target.nextElementSibling.innerHTML = "OK!";
   } else {
     userIdSpan.style.color = "red";
     userIdSpan.innerHTML =
       "첫글자는 반드시 영문자로, 그리고 영문자, 숫자 포함하여 총 4~12자로 입력하시오.";
-    //e.target.nextElementSibling.style.color = "red";
-    //e.target.nextElementSibling.innerHTML =
-    //("첫글자는 반드시 영문자로, 그리고 영문자, 숫자 포함하여 총 4~12자로 입력하시오.");
   }
 });
 
 // 영문자, 숫자, 특수문자 포함하여 총 8~15자로 입력하시오.
 // [!-~]{여기도 여러분들이 채워주세요!}
-userPwd.addEventListener("input", function (e) {
+userPwd.addEventListener("input", function () {
   const regExp = /^[!-~]{8,15}$/;
   const check = regExp.test(userPwd.value);
 
@@ -46,20 +36,19 @@ userPwd.addEventListener("input", function (e) {
   } else {
     userPwdSpan.style.color = "red";
     userPwdSpan.innerHTML =
-      "영문자, 숫자, 특수문자 포함하여 총 8~15자로 입력하시오.!";
+      "영문자, 숫자, 특수문자 포함하여 총 8~15자로 입력하시오.";
   }
 });
 
 // 위의 비밀번호와 일치하게 입력하시오.
 // 정규표현식 필요 X, 위에 입력한 비밀번호 값이랑 비교!
-// userPwd.value === userPwdCheck.value
 userPwdCheck.addEventListener("input", function () {
   if (userPwd.value === userPwdCheck.value) {
     userPwdCheckSpan.style.color = "green";
     userPwdCheckSpan.innerHTML = "OK!";
   } else {
     userPwdCheckSpan.style.color = "red";
-    userPwdCheckSpan.innerHTML = "위의 비밀번호와 일치하게 입력하시오.!";
+    userPwdCheckSpan.innerHTML = "위의 비밀번호와 일치하게 입력하시오.";
   }
 });
 
@@ -82,15 +71,8 @@ userName.addEventListener("input", function () {
 // 이메일 형식에 맞춰서 입력하시오.
 // abc123!@gmail.com
 // -> (영어나숫자여러문자상관없이 길이도 상관없이 : +)@(앞이랑 마찬가지)
-email.addEventListener("input", function () {
-  const regExp = /^[!-~]+@[!-~]+$/;
-  const check = regExp.test(email.value);
 
-  if (check) {
-    emailSpan.style.color = "green";
-    emailSpan.innerHTML = "OK!";
-  } else {
-    emailSpan.style.color = "red";
-    emailSpan.innerHTML = "이메일 형식에 맞춰서 입력하시오.";
-  }
+email.addEventListener("input", function () {
+  const regExp = /^$/;
+  const check = regExp.test(email.value);
 });
